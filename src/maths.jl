ab9825070f5d2a71908d7446f5f8a4cb669dbd2d
Jonathan7773 <jonathan.e.laursen@gmail.com>
2024-04-05 13:01:50 +0200

    test2

--- src/maths.jl
+++ src/maths.jl
@@ -121,7 +121,10 @@ function spm_cross(x, y=nothing; remove_singleton_dims=true, args...)
         #if y isa AbstractVector
         #    y = spm_cross(y)
         #end
-
+        if y isa AbstractVector
+            y = reduce((a, b) -> spm_cross(a, b; remove_singleton_dims=remove_singleton_dims), y)
+        end
+        
         reshape_dims_x = tuple(size(x)..., ones(Int, ndims(y))...)
         A = reshape(x, reshape_dims_x)
 
